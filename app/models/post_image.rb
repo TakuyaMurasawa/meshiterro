f9588423b430bfd5604c99d0007b78d3b518c5a2
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2021-06-14 11:33:13 +0000

    [FINISH]4-4

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,4 +2,15 @@ class PostImage < ApplicationRecord
   belongs_to :user
   
   attachment :image
+  
+  # 一 : N = post_image : post_comments(モデル名の複数形)
+  has_many :post_comments, dependent: :destroy
+  
+  
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+  
 end
